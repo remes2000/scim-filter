c7ae94a5019ab52e63d5c537935e28181f1f8b80
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2025-07-08 22:00:28 +0200

    improve lexer

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -1,9 +1,10 @@
-type LexerState = 'start' | 'operator' | 'value' | 'stringValue' | 'logicalOperator';
+type LexerState = 'filter' | 'operator' | 'value' | 'stringValue' | 'logicalOperator' | 'ignoreSpace'
 
 type Token = IdentifierToken | StringValueToken | BooleanValueToken | NumberValueToken | NullValueToken | OperatorToken | LogicalOperatorToken | EOTToken | OpenParenthesis | CloseParenthesis;
 type Operator = 'eq' | 'ne' | 'co' | 'sw' | 'ew' | 'pr' | 'gt' | 'ge' | 'lt' | 'le';
 type LogicalOperator = 'and' | 'or' | 'not';
 
+// TODO: call those COMPARE_OPERATORS
 const VALUE_REQUIRED_OPERATORS = ['eq', 'ne', 'co', 'sw', 'ew', 'gt', 'ge', 'lt', 'le'];
 const NO_VALUE_OPERATORS = ['pr'];
 const OPERATORS = [...VALUE_REQUIRED_OPERATORS, ...NO_VALUE_OPERATORS];
@@ -64,111 +65,145 @@ interface EOTToken {
 }
 
 class Lexer {
-  private currentState: LexerState = 'start';
-  private currentChar: string | null = null;
-  private memory: string = '';
-  private readonly tokens: Token[] = [];
+  private state: State = new FilterState([]);
 
-  constructor(private readonly input: string) {}
+  constructor(private readonly input: string) { }
 
   parse(): Token[] {
-    [ ...this.input.split(''), null ].forEach((char) => {
-      this.currentChar = char;
-      if (this.currentState === 'start') {
-        this.handleStart();
-      } else if (this.currentState === 'operator') {
-        this.handleOperator();
-      } else if (this.currentState === 'value') {
-        this.handleValue();
-      } else if (this.currentState === 'stringValue') {
-        this.handleStringValue()
-      }
-      else if (this.currentState === 'logicalOperator') {
-        this.handleLogicalOperator();
-      }
+    [...this.input.split(''), null].forEach((char) => {
+      this.state = this.state.handle(char);
+      console.log(this.state.tokens);
     });
-    return [...this.tokens, { type: 'EOT' }];
+    return [...this.state.tokens, { type: 'EOT' }];
   }
+}
 
-  private setState(newState: LexerState) {
-    this.currentState = newState;
-    this.memory = '';
-  }
+interface State {
+  handle: (char: string | null) => State;
+  readonly tokens: Token[];
+}
 
-  private handleStart() {
-    /* TODO: For now I've wrongly assumed that it has to start with an indentifier */
-    if (this.currentChar === SP || this.currentChar === null) {
-      /* TODO: verify that identifier is a valid value */
+class FilterState implements State {  
+  private memory = '';
+
+  constructor(readonly tokens: Token[]) {};
+
+  handle(char: string | null) {
+    if (char === SP || char === null) {
       this.tokens.push({ type: 'Identifier', value: this.memory });
-      this.setState('operator');
-    } else {
-      this.memory += this.currentChar;
+      return new OperatorState(this.tokens);
     }
+    if (char === '(') {
+      this.tokens.push({ type: 'OpenParenthesis', value: '(' });
+      return new FilterState(this.tokens);
+    }
+    if (char === ')') {
+      this.tokens.push({ type: 'CloseParenthesis', value: ')' });
+      return new LogicalOperatorState(this.tokens);
+    }
+    this.memory += char;
+    return this;
   }
+}
 
-  private handleOperator() {
-    if (this.currentChar === SP || this.currentChar === null) {
+class OperatorState implements State {
+  private memory: string = '';
+
+  constructor(readonly tokens: Token[]) {}
+
+  handle(char: string | null) {
+    if (char === SP || char === null) {
       const operator = this.memory;
       if (!isOperator(operator)) {
-        /* TODO: Define good error type */
         throw new Error(`${this.memory} is not a valid operator`);
       }
+
       this.tokens.push({ type: 'Operator', value: operator });
-      if (VALUE_REQUIRED_OPERATORS.includes(operator)) {
-        this.setState('value');
-      } else {
-        this.setState('logicalOperator');
-      }
-    } else {
-      this.memory += this.currentChar;
+      return VALUE_REQUIRED_OPERATORS.includes(operator) ? new ValueState(this.tokens) : new LogicalOperatorState(this.tokens);
     }
-  }
 
-  private handleValue() {
-    if (this.currentChar === SP || this.currentChar === null) {
+    this.memory += char;
+    return this;
+  }
+}
+
+class ValueState implements State {
+  private memory: string = '';
+
+  constructor(readonly tokens: Token[]) {}
+
+  handle(char: string | null) {
+    if (char === SP || char === null) {
       /* TODO: handle nulls and booleans */
-      this.setState('logicalOperator');
-    } if (this.memory === '' && this.currentChar === QUOTE) {
-      this.setState('stringValue');
+      return new LogicalOperatorState(this.tokens);
+    } if (this.memory === '' && char === QUOTE) {
+      return new StringValueState(this.tokens);
     } else {
-      this.memory += this.currentChar;
+      this.memory += char;
+      return this;
     }
   }
+}
 
-  private handleStringValue() {
-    if (this.currentChar === QUOTE && this.memory.slice(-1) !== '\\') {
+class StringValueState implements State {
+  private memory: string = '';
+
+  constructor(readonly tokens: Token[]) {}
+
+  handle(char: string | null) {
+    if (char === QUOTE && this.memory.slice(-1) !== '\\') {
       // TODO: I think I should replace escaped quotes with only a quote
       this.tokens.push({ type: 'String', value: this.memory });
-      this.setState('logicalOperator');
-    } else if (this.currentChar === null) {
+      return new LogicalOperatorState(this.tokens);
+    } else if (char === null) {
       /* TODO: add better error type */
       throw new Error('Unterminated string literal');
     } else {
-      this.memory += this.currentChar;
+      this.memory += char;
+      return this;
     }
   }
+}
 
-  private handleLogicalOperator() {
-    if (this.currentChar === null) {
-      if (this.memory !== '') {
-        /* TODO: add better types for errors */
-        throw new Error('Unexpected end of input while parsing logical operator');
-      } else {
-        return;
+class LogicalOperatorState implements State {
+  private memory = '';
+
+  constructor(readonly tokens: Token[]) {}
+
+  handle(char: string | null) {
+    if (char === null) {
+      if (this.memory === '') {
+        // End of input :)
+        return this;
       }
+      // TODO: Define good error type
+      throw new Error(`Unexpected end of input, expected logical operator, got ${this.memory}`);
     }
-    if (this.currentChar === SP) {
+
+    if (char === SP) {
+      if (this.memory === '') {
+        // Ignore space, how can I detect multiple spaces? TODO
+        return this;
+      }
+
       const operator = this.memory;
       if (!isLogicalOperator(operator)) {
         // TODO: Define good error type
         throw new Error(`${operator} is not a valid logical operator`);
       }
       this.tokens.push({ type: 'LogicalOperator', value: operator });
-      /* TODO: it should not be start */
-      this.setState('start');
-    } else {
-      this.memory += this.currentChar;
+      return new FilterState(this.tokens);
     }
+
+    if (char === ')') {
+      if (this.memory === '') {
+        this.tokens.push({ type: 'CloseParenthesis', value: ')' });
+        return new LogicalOperatorState(this.tokens);
+      }
+    }
+
+    this.memory += char;
+    return this;
   }
 }
 
